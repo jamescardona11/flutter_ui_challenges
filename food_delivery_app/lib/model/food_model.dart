002e91d976b7dd3d1634d008005a19db47bd9202
jamescardona11 <jjcardonao@gmail.com>
2020-07-11 19:42:57 -0500

    Data Model
    - Data Model (OK)
    - TopBar
    - ViewPagerTop
      - item
    - BottomList
      - item

---
+++ food_delivery_app/lib/model/food_model.dart
@@ -0,0 +1,19 @@
+class FoodModel {
+  final String name;
+  final double price;
+  final double weight;
+  final double calory;
+  final double protein;
+  final String item;
+  final String imgPath;
+
+  FoodModel({
+    this.name,
+    this.price,
+    this.weight,
+    this.calory,
+    this.protein,
+    this.item,
+    this.imgPath,
+  });
+}
