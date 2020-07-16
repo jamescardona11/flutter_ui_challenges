c2170db7166bdd956f3f951677727036ee3fc85a
jamescardona11 <jjcardonao@gmail.com>
2020-07-15 19:12:39 -0500

    init drawing app

---
+++ custom_painting_app/android/app/src/main/kotlin/co/kodevincere/custom_painting_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.kodevincere.custom_painting_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ custom_painting_app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        body: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: const Text('Home Page'),
+      ),
+    );
+  }
+}
+
+class DrawingArea {
+  Offset point;
+  Paint areaPaint;
+}
