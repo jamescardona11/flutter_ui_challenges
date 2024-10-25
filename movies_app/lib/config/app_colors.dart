89681300f08d5048d71f9c16377a7c9c84375c29
jamescardona11 <65878592+jamescardona11@users.noreply.github.com>
2024-10-24 21:05:43 -0500

    AppColors

---
+++ movies_app/lib/config/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primaryColor = Color(0xff1761DF);
+  static const Color secondaryColor = Color(0xffF5C518);
+}
